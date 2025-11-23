0cab447eddef484fb4b34963bbfde994dcda1423
M4SIH <amir.hoseyn.hamidi@gmail.com>
2025-11-23 18:12:44 +0330

    Improve debounce function with leading feature

--- debounce.js
+++ debounce.js
@@ -1,18 +1,24 @@
-function debounce(fn, wait, { leading = false }) {
-  let timer;
-  let count = 0;
-  if (leading && !timer) {
-    return function (...args) {
-      fn.apply(this, args);
-    };
-  } else {
-    return function (...args) {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        fn.apply(this, args);
-      }, wait);
-    };
-  }
+function debounce(fn, wait, { leading = false } = {}) {
+  let timer = null;
+  let lastArgs = null;
+  let lastThis = null;
+  let hasLeadingRun = false;
+
+  return function (...args) {
+    lastArgs = args;
+    lastThis = this;
+
+    if (leading && !timer && !hasLeadingRun) {
+      hasLeadingRun = true;
+      return fn.apply(lastThis, lastArgs);
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      hasLeadingRun = false;
+      fn.apply(lastThis, lastArgs);
+      timer = null;
+    }, wait);
+  };
 }
 
 function hi() {
